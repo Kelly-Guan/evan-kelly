bebb47446fa0d849f8b31a046b0f03fec10a8b49
Kelly Guan <kgguan@uwaterloo.ca>
2025-07-21 18:21:33 -0400

    added emoji

--- src/screens/EvanSweeper.tsx
+++ src/screens/EvanSweeper.tsx
@@ -5,7 +5,7 @@ export default function EvanSweeper() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-purple-200">
-      <h1 className="text-4xl my-5 text-purple-900">evan sweeper</h1>
+      <h1 className="text-4xl my-5 text-purple-900">evan sweeper 🧹</h1>
       <Boards />
       </div>
     </>
